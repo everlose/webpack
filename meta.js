f2bc25a1976abee9dbdece40bf3d3caa16c3ba0b
陈子云 <chentongta@souche.com>
2018-03-22 15:14:04 +0800

    chore: delete console.log

--- meta.js
+++ meta.js
@@ -180,7 +180,6 @@ module.exports = {
     }) {
         const green = chalk.green
 
-        console.log(green)
         sortDependencies(data, green)
 
         const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
